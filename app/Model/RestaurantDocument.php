679fe12e1d52873cc9af409537773cfd9c737cbb
kanfur <kanfur78@gmail.com>
2020-02-22 16:21:14 +0300

    restaurantDocument model has filled

--- app/Model/RestaurantDocument.php
+++ app/Model/RestaurantDocument.php
@@ -3,8 +3,20 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class RestaurantDocument extends Model
 {
+    use SoftDeletes;
     //
+    protected $fillable = [
+        'user_id','restaurant_id','address','personnel','title','tax_administration','tax_no','tic_sic_no','mersis_no'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function restaurant(){
+        return $this->belongsTo(Restaurant::class);
+    }
 }
