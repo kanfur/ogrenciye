6e75a6f97b14be324de313d89a41e26cce9cb4b6
kanfur <kanfur78@gmail.com>
2020-02-23 16:06:33 +0300

    menu resource fixed

--- app/Http/Resources/MenuResource.php
+++ app/Http/Resources/MenuResource.php
@@ -17,7 +17,7 @@ class MenuResource extends JsonResource
         return [
             'id' => $this->id,
             'restaurant_id' => $this->restaurant_id,
-            'restaurant' => new RestaurantResource($this->restaurant()),
+            'restaurant' => new RestaurantResource($this->restaurant),
             'description' => $this->description,
             'menu_date' => $this->menu_date,
             'apply_limit' => $this->apply_limit?$this->apply_limit:5,
